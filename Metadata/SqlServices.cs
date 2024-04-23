b95f300bc7523b1ea5534327ab5c127787ef91b5
mnln <martin.loxton@nhs.net>
2024-04-23 13:45:25 +0100

    auditing part 2

--- Metadata/SqlServices.cs
+++ Metadata/SqlServices.cs
@@ -7,6 +7,7 @@ using static System.Net.Mime.MediaTypeNames;
 using System.Data;
 using WMFACS_Review_List.Models;
 using System.Xml;
+using System.Numerics;
 
 namespace WMFACS_Review_List.Metadata
 {
@@ -27,29 +28,104 @@ namespace WMFACS_Review_List.Metadata
         {
             string cmdText="";
             string dateEdited = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            string adminOldValue = "";
+            string gcOldValue = "";
+            string consultantOldValue = "";
 
-            if (areaCode != null)
-            {               
-                cmdText = $"Update ListAreaNames set FHCStaffCode='{admin}', GC='{gc}', ConsCode='{consultant}', WhoLastEdit='{staffCode}', " +
-                    $"DateLastEdit='{dateEdited}' where AreaCode='{areaCode}'";
-            }
-            else
-            {                
-                cmdText = $"Update ListAreaNames set FHCStaffCode='{admin}', GC='{gc}', ConsCode='{consultant}', WhoLastEdit='{staffCode}', " +
-                    $"DateLastEdit='{dateEdited}' where AreaID={areaID}";
-            }
-
+            cmdText = $"select FHCStaffCode, GC, ConsCode from ListAreaNames where AreaID = {areaID}";
             _cmd.CommandText = cmdText;
             _con.Open();
-            _cmd.ExecuteNonQuery();
+            using (var reader = _cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    adminOldValue = reader.GetString("FHCStaffCode");
+                    gcOldValue = reader.GetString("GC");
+                    consultantOldValue = reader.GetString("ConsCode");
+                }
+            }
             _con.Close();
 
-            CreateAudit(staffCode, "WMFACSUpdateAreas", areaID);
+            List<int> areaIDs = new List<int>();
+
+            if (areaCode != null)
+            {
+                cmdText = $"select AreaID from ListAreaNames where AreaCode = '{areaCode}'";
+                _cmd.CommandText = cmdText;
+                _con.Open();
+                using (var reader = _cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        areaIDs.Add(reader.GetInt32("AreaID"));
+                    }
+                }
+                _con.Close();
+            }
+            else
+            {
+                areaIDs.Add(areaID);
+            }
+
+            int bleh = areaIDs.Count;
+
+            //if (areaCode != null)
+            //{               
+            //    cmdText = $"Update ListAreaNames set FHCStaffCode='{admin}', GC='{gc}', ConsCode='{consultant}', WhoLastEdit='{staffCode}', " +
+            //        $"DateLastEdit='{dateEdited}' where AreaCode='{areaCode}'";
+            //}
+            //else
+            //{
+            foreach (var aid in areaIDs)
+            {
+                cmdText = $"Update ListAreaNames set FHCStaffCode='{admin}', GC='{gc}', ConsCode='{consultant}', WhoLastEdit='{staffCode}', " +
+                    $"DateLastEdit='{dateEdited}' where AreaID={aid}";
+
+                //}
+
+                _cmd.CommandText = cmdText;
+                _con.Open();
+                _cmd.ExecuteNonQuery();
+                _con.Close();
+
+                CreateAudit(staffCode, "WMFACSUpdateAreas", aid);
+
+                if (adminOldValue != admin)
+                {
+                    WriteAuditUpdate(staffCode, "FHCStaffCode", aid, adminOldValue, admin, System.Environment.MachineName, "ListAreaNames");
+                }
+                if (gcOldValue != gc)
+                {
+                    WriteAuditUpdate(staffCode, "GC", aid, gcOldValue, gc, System.Environment.MachineName, "ListAreaNames");
+                }
+                if (consultantOldValue != consultant)
+                {
+                    WriteAuditUpdate(staffCode, "ConsCode", aid, consultantOldValue, consultant, System.Environment.MachineName, "ListAreaNames");
+                }
+            }
         }
 
         public void UpdateReferralStatus(string complete, string adminStatus, string staffCode, int id)
         {
-            string cmdText = $"Update MasterActivityTable set COMPLETE='{complete}', Status_Admin='{adminStatus}', " +
+            string cmdText;
+            string completeOldValue = "";
+            string adminStatusOldValue = "";
+            
+            cmdText = $"select complete, status_admin from masteractivitytable where refid = {id}";
+            _cmd.CommandText = cmdText;
+            _con.Open();
+            using (var reader = _cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    completeOldValue = reader.GetString("COMPLETE");
+                    adminStatusOldValue = reader.GetString("Status_Admin");
+                }
+            }
+            _con.Close();
+
+
+            cmdText = $"Update MasterActivityTable set COMPLETE='{complete}', Status_Admin='{adminStatus}', " +
                 $"updateddate = '{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}', updatedby='{staffCode}' where RefID={id}";
             
             _cmd.CommandText = cmdText;
@@ -58,6 +134,14 @@ namespace WMFACS_Review_List.Metadata
             _con.Close();
 
             CreateAudit(staffCode, "WMFACSUpdateReferralStatus", id);
+            if (complete != completeOldValue)
+            {
+                WriteAuditUpdate(staffCode, "COMPLETE", id, completeOldValue, complete, System.Environment.MachineName, "MasterActivityTable");
+            }
+            if (adminStatus != adminStatusOldValue)
+            {
+                WriteAuditUpdate(staffCode, "Status_Admin", id, adminStatusOldValue, adminStatus, System.Environment.MachineName, "MasterActivityTable");
+            }
         }
         public void CreateAudit(string staffCode, string formName, int recordPrimaryKey)
         {
@@ -71,7 +155,21 @@ namespace WMFACS_Review_List.Metadata
             _con.Close();
         }
 
-
+        public void WriteAuditUpdate(string staffCode, string fieldName, int recordPrimaryKey, string oldValue, string newValue, string machineName, string tableName)
+        {
+            _con.Open();
+            SqlCommand cmd = new SqlCommand("dbo.sp_WriteAuditUpdate", _con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@staffCode", SqlDbType.VarChar).Value = staffCode;
+            cmd.Parameters.Add("@form", SqlDbType.VarChar).Value = fieldName;
+            cmd.Parameters.Add("@recordkey", SqlDbType.VarChar).Value = recordPrimaryKey.ToString();
+            cmd.Parameters.Add("@oldValue", SqlDbType.VarChar).Value = oldValue;
+            cmd.Parameters.Add("@newValue", SqlDbType.VarChar).Value = newValue;
+            cmd.Parameters.Add("@machineName", SqlDbType.VarChar).Value = machineName;
+            cmd.Parameters.Add("@tableName", SqlDbType.VarChar).Value = tableName;
+            cmd.ExecuteNonQuery();
+            _con.Close();
+        }
 
         
          
